afdd01644ab85619ece1bfe33f17747db8825d55
Joao Breno Gomes <joaobrenogs@gmail.com>
2023-08-27 15:09:16 -0300

    Update ex76.py

--- ProjetosPy/ex76.py
+++ ProjetosPy/ex76.py
@@ -1,16 +1,16 @@
-listagem = ("Cerveja", 7.00,
-             "Cachaca", 6.00,
-             "Vodka", 9.00,
-             "Espeto", 5.00,
-             "Carne assada", 25.00,
-             "Caldinho", 6.00,
-             "Macaxeira", 3.00,
-             "Batatinha", 10.00)
+listagem = ("1","Cerveja", 7.00,
+            "2","Cachaca", 6.00,
+            "3","Vodka", 9.00,
+            "4","Espeto", 5.00,
+            "5","Carne assada", 25.00,
+            "6","Caldinho", 6.00,
+            "7","Macaxeira", 3.00,
+            "8","Batatinha", 10.00)
 print("-" *30)
 print("LISTAGEM DE PRECOS")
 print("-" *30)
 for pos in range(0,len(listagem)):
-    if pos %2 == 0:
+    if pos %3 == 0:
         print(f'{listagem[pos]:.<30}',end="")
     else:
         print(f"R${listagem[pos]:>5}")
